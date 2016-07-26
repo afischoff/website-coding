ec17ede5acbf5918ca59a535f5bb5f98b8d16a4f
Andy Fischoff <afischoff@salesforce.com>
2016-07-25 21:57:05 -0500

    convert to PDO

--- 08_JavaScript/citiesApi.php
+++ 08_JavaScript/citiesApi.php
@@ -5,11 +5,7 @@ $state = urldecode($_GET['state']);
 $city = urldecode($_GET['city']);
 
 // connect to db
-$db = mysqli_connect("localhost", "afischof_class", "i}6n+{XbFu-#", "afischof_websites_class");
-if (mysqli_connect_errno()) {
-	printf("Connect failed: %s\n", mysqli_connect_error());
-	exit();
-}
+$db = new PDO("mysql:dbname=afischof_websites_class", "afischof_class", "i}6n+{XbFu-#");
 
 // search database based on input params
 // prepare the statements
@@ -19,28 +15,24 @@ if (empty($city) && empty($state)) {
 
 } else if (empty($city) && !empty($state)) {
 	// search by state
-	$statement = $db->prepare("select distinct(city) from cities where state = ? order by city");
-	$statement->bind_param('s', $state);
+	$statement = $db->prepare("select distinct(city) from cities where state = :state order by city");
+	$statement->bindParam(':state', $state);
 
 } else {
 	// search by city and state
-	$statement = $db->prepare("select zip from cities where state = ? and city = ? order by zip");
-	$statement->bind_param('ss', $state, $city);
+	$statement = $db->prepare("select zip from cities where state = :state and city = :city order by zip");
+	$statement->bindParam(':state', $state);
+	$statement->bindParam(':city', $city);
 }
 
 // run the statements
 $statement->execute();
-$result = $statement->get_result();
 
 // build response array
 $output = [];
-while ($row = $result->fetch_row()) {
+while ($row = $statement->fetch()) {
 	$output[] = current($row);
 }
-
-// close the connection
-$statement->close();
-$db->close();
 
 // output the results
 header('Content-Type: application/json');
